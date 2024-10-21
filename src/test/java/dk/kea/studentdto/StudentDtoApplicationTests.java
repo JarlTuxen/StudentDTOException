e961473714885e4274394aa0883a58f3c84c9577
Jarl Tuxen <jart@kea.dk>
2024-10-21 15:43:20 +0200

    added password to Bruce

---
+++ src/test/java/dk/kea/studentdto/StudentDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.studentdto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
